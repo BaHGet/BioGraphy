ad4072bf33646ec22b5a64a8fd2d99e42bc22e23
Abdul ELrahman A.Bahget <62491215+BaHGet@users.noreply.github.com>
2021-07-13 15:48:03 +0200

    commit main

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from cs50 import SQL
 from tempfile import mkdtemp
 
@@ -17,12 +16,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route('/')
 def Home_Page():
